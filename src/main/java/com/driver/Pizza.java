f69b0ea755314e5e5df3ba44beb93857b88a0ed5
dayalu <dayalupatel98@gmail.com>
2022-10-19 21:16:21 +0530

    code completed

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,26 +3,30 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
-    private String bill;
-    private boolean isTakeAwayAdded = false;
-    private int takeCharge;
-    private int totalTakeCharge;
-    private boolean isCheezeAdded = false;
     private int cheezeCharge;
-    private int totalCheezeCharge;
-    private boolean isToppingAdded = false;
-    private int toppingCharge;
-    private int totalToppingCharge;
-    private String[] billArr =  new String[5];
+    private int toppingsCharge;
+    private int takeAwayCharge;
+    private String bill;
+    private boolean isVeg;
+    private boolean isBillGenerated;
+    private boolean isCheezeAdded;
+    private boolean isToppingAdded;
+    private boolean isTakeAwayAdded;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        price = isVeg ? 300 : 400;
-        cheezeCharge = 80;
-        toppingCharge = isVeg ? 70 : 120;
-        takeCharge = 20;
+        this.price = isVeg ? 300 : 400;
+        this.cheezeCharge = 80;
+        this.toppingsCharge = isVeg ? 70 : 120;
+        this.takeAwayCharge = 20;
+
+        this.isCheezeAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeAwayAdded = false;
+        this.isBillGenerated = false;
+
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
     public int getPrice(){
         return this.price;
@@ -30,24 +34,16 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if(isCheezeAdded) {
-            totalCheezeCharge += cheezeCharge;
-            billArr[1] = "Extra Cheese Added: " + totalCheezeCharge;
-        } else {
-            totalCheezeCharge = cheezeCharge;
-            billArr[1] = "Extra Cheese Added: " + cheezeCharge;
+        if(isCheezeAdded==false) {
+            this.price += this.cheezeCharge;
             isCheezeAdded = true;
         }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(isToppingAdded) {
-            totalToppingCharge += toppingCharge;
-            billArr[2] = "Extra Toppings Added: " + toppingCharge;
-        } else {
-            totalToppingCharge = toppingCharge;
-            billArr[2] = "Extra Toppings Added: " + toppingCharge;
+        if(isToppingAdded==false) {
+            this.price += this.toppingsCharge;
             isToppingAdded = true;
         }
     }
@@ -55,26 +51,27 @@ public class Pizza {
     public void addTakeaway(){
         // your code goes here
         if(!isTakeAwayAdded) {
-            totalTakeCharge = takeCharge;
-            billArr[3] =  "Paperbag Added: " + takeCharge;
+            this.price += this.takeAwayCharge;
             isTakeAwayAdded = true;
         }
     }
 
     public String getBill(){
         // your code goes here
-        billArr[0] = "Base Price Of The Pizza: " + price;
+        if(isBillGenerated==false) {
+            if(isCheezeAdded) {
+                this.bill += "Extra Cheese Added: " + this.cheezeCharge + "\n";
+            }
+            if(isToppingAdded) {
+                this.bill += "Extra Toppings Added: " + this.toppingsCharge + "\n";
+            }
+            if(isTakeAwayAdded) {
+                this.bill += "Paperbag Added: " + this.takeAwayCharge + "\n";
+            }
+            this.bill += "Total Price: " + this.price + "\n";
 
-        bill = billArr[0];
-        for(int i=1;i<5;i++) {
-            if( billArr[i] != null )
-               bill = bill + "\n" + billArr[i];
+            isBillGenerated = true;
         }
-        int total = price + totalCheezeCharge + totalToppingCharge + totalTakeCharge;
-        billArr[4] = "Total Price: " + total;
-
-        bill = bill + "\n" + billArr[4];
-
         return this.bill;
     }
 }
